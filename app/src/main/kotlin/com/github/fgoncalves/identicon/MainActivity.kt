a5730c06ff77b6ffb37f3d57589726e5beaf27c4
Fred <fgoncalves@babbel.com>
2017-10-29 18:27:02 +0100

    Sharing only after permissions are granted

--- app/src/main/kotlin/com/github/fgoncalves/identicon/MainActivity.kt
+++ app/src/main/kotlin/com/github/fgoncalves/identicon/MainActivity.kt
@@ -1,21 +1,51 @@
 package com.github.fgoncalves.identicon
 
+import android.content.Intent
 import android.databinding.DataBindingUtil
+import android.databinding.Observable
 import android.os.Bundle
+import android.support.v4.view.MenuItemCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.ShareActionProvider
+import android.view.Menu
 import com.github.fgoncalves.identicon.databinding.ActivityMainBinding
 import com.github.fgoncalves.identicon.lib.IdenticonImpl
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 class MainActivity : AppCompatActivity() {
+    private lateinit var shareActionProvider: ShareActionProvider
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val viewModel = MainViewModelImpl(IdenticonImpl())
+        val viewModel = MainViewModelImpl(this, IdenticonImpl())
 
         binding.viewModel = viewModel
+
+        viewModel.shareIntent.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(observable: Observable?, propertyId: Int) {
+                setShareIntent(viewModel.shareIntent.get())
+            }
+        })
+
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+
+        val item = menu.findItem(R.id.menu_item_share)
+        shareActionProvider = MenuItemCompat.getActionProvider(item) as ShareActionProvider
+
+        return true
+    }
+
+    private fun setShareIntent(shareIntent: Intent?) {
+        shareActionProvider.setShareIntent(shareIntent)
     }
 }
--- app/src/main/kotlin/com/github/fgoncalves/identicon/MainViewModel.kt
+++ app/src/main/kotlin/com/github/fgoncalves/identicon/MainViewModel.kt
@@ -1,9 +1,12 @@
 package com.github.fgoncalves.identicon
 
+import android.content.Context
+import android.content.Intent
 import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
 import android.databinding.ObservableInt
 import android.graphics.Bitmap
+import android.net.Uri
 import android.util.Log
 import android.view.View
 import com.github.fgoncalves.identicon.lib.Identicon
@@ -15,22 +18,26 @@ interface MainViewModel {
 
     val imageVisibility: ObservableInt
 
-    val imageBitmap: ObservableField<Bitmap>
+    val imageBitmap: ObservableField<Bitmap?>
 
     val text: ObservableField<String>
 
     val generateButtonEnabled: ObservableBoolean
 
+    val shareIntent: ObservableField<Intent?>
+
     fun onGenerateButtonClicked(view: View)
 }
 
 class MainViewModelImpl(
+        val context: Context,
         val identicon: Identicon) : MainViewModel {
     override val progressVisibility = ObservableInt(View.GONE)
     override val imageVisibility = ObservableInt(View.VISIBLE)
-    override val imageBitmap = ObservableField<Bitmap>(null)
+    override val imageBitmap = ObservableField<Bitmap?>(null)
     override val text = ObservableField<String>("")
     override val generateButtonEnabled = ObservableBoolean(true)
+    override val shareIntent = ObservableField<Intent?>(null)
 
     override fun onGenerateButtonClicked(view: View) {
         generateState()
@@ -40,12 +47,22 @@ class MainViewModelImpl(
                         {
                             normalState()
                             imageBitmap.set(it)
+                            setShareIntent(it)
                         },
                         {
                             normalState()
                             imageBitmap.set(null)
                             Log.e("MainViewModelImpl", "Failed to generate bitmap", it)
                         })
+    }
+
+    private fun setShareIntent(bitmap: Bitmap) {
+        val bitmapPath = PhotoUtils.insertImage(context.contentResolver, bitmap, "${text.get()}.png", "Identicon for ${text.get()}")
+        val bitmapUri = Uri.parse(bitmapPath)
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "image/png"
+        intent.putExtra(Intent.EXTRA_STREAM, bitmapUri)
+        shareIntent.set(intent)
     }
 
     private fun generateState() {
