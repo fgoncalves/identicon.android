a98e53e92940f8c2a7f2e11fb5c73291dc20136f
Fred <fgoncalves@babbel.com>
2017-10-29 20:04:38 +0100

    Adds more tests

--- identiconlib/src/test/kotlin/com/github/fgoncalves/identicon/IdenticonTest.kt
+++ identiconlib/src/test/kotlin/com/github/fgoncalves/identicon/IdenticonTest.kt
@@ -1,5 +1,7 @@
 package com.github.fgoncalves.identicon
 
+import android.graphics.Color
+import com.github.fgoncalves.identicon.lib.*
 import org.amshove.kluent.`should equal to`
 import org.junit.Test
 
@@ -21,6 +23,62 @@ class IdenticonTest {
         val result = DropLastByteFunction.apply(identicon)
 
         result.bitmapPoints.toHex() `should equal to` "00"
+    }
+
+    @Test
+    fun `get colour should take colour from the first 3 bytes as rgb`() {
+        val identicon = IdenticonData(bitmapPoints = listOf(
+                IdenticonBitmapPoint(value = 0),
+                IdenticonBitmapPoint(value = 0xFF.toByte()),
+                IdenticonBitmapPoint(value = 0xEE.toByte())))
+
+        val result = GetColourFunction.apply(identicon)
+
+        result.color `should equal to` Color.rgb(0, 0xFF.toByte().toInt(), 0xEE.toByte().toInt())
+    }
+
+    @Test
+    fun `mirror function should repeat the same values after the vertical axis`() {
+        val identicon = IdenticonData(bitmapPoints = listOf(
+                IdenticonBitmapPoint(value = 0),
+                IdenticonBitmapPoint(value = 0xFF.toByte()),
+                IdenticonBitmapPoint(value = 0xEE.toByte())))
+
+        val result = MirrorFunction.apply(identicon)
+
+        result.bitmapPoints[0].value `should equal to` 0
+        result.bitmapPoints[1].value `should equal to` 0xFF.toByte()
+        result.bitmapPoints[2].value `should equal to` 0xEE.toByte()
+        result.bitmapPoints[3].value `should equal to` 0xFF.toByte()
+        result.bitmapPoints[4].value `should equal to` 0
+    }
+
+    @Test
+    fun `with indexes function should put indexes into the points`() {
+        val identicon = IdenticonData(bitmapPoints = listOf(
+                IdenticonBitmapPoint(value = 0),
+                IdenticonBitmapPoint(value = 0xFF.toByte()),
+                IdenticonBitmapPoint(value = 0xEE.toByte())))
+
+        val result = WithIndexesFunction.apply(identicon)
+
+        result.bitmapPoints[0].value `should equal to` 0
+        result.bitmapPoints[1].index `should equal to` 1
+        result.bitmapPoints[2].index `should equal to` 2
+    }
+
+    @Test
+    fun `filter function should filter out odd values`() {
+        val identicon = IdenticonData(bitmapPoints = listOf(
+                IdenticonBitmapPoint(value = 0),
+                IdenticonBitmapPoint(value = 0xFF.toByte()),
+                IdenticonBitmapPoint(value = 0xEE.toByte())))
+
+        val result = FilterEvenValuesFunction.apply(identicon)
+
+        result.bitmapPoints.size `should equal to` 2
+        result.bitmapPoints[0].value `should equal to` 0
+        result.bitmapPoints[1].value `should equal to` 0xEE.toByte()
     }
 
     private val HEX_CHARS = "0123456789ABCDEF"
